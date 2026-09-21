c67af6a88ecfbf8842a99311f7de9ee48d7f8681
Mist-Wolf <mazimbabwalya@gmail.com>
2026-09-21 09:50:43 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   // optimizeDeps: {
   //   exclude: ['lucide-react'],
   // },
-   base: "/developer-portfolio/",
+   base: "/davies-mazimba-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
